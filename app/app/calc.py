24688fcbffd69f087557c46d2e527a0618da1cbe
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-02-04 01:24:57 +0600

    chore: postgresql setup working with docker

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    # add x and y and return result
+    return x + y
+
+
+def subtract(low, high):
+    # subtract x from y and return result
+    return high - low
